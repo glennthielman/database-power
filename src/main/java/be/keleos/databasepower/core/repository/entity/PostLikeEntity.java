601fd619c0d7b201d5983ea42b8e62bb3396b657
glennthielman <37749736+glennthielman@users.noreply.github.com>
2025-06-26 14:43:39 +0200

    updated template and cleanup

--- src/main/java/be/keleos/databasepower/core/repository/entity/PostLikeEntity.java
+++ src/main/java/be/keleos/databasepower/core/repository/entity/PostLikeEntity.java
@@ -1,6 +1,9 @@
 package be.keleos.databasepower.core.repository.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import org.hibernate.annotations.Filter;
 import org.hibernate.annotations.FilterDef;
 
